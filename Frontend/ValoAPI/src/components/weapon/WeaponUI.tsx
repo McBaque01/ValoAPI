2d5b496996e9dcd77418a3212e9db04530908d88
McBaque <mcbaque01@gmail.com>
2024-04-29 23:40:58 +0800

    commiting again!

--- Frontend/ValoAPI/src/components/weapon/WeaponUI.tsx
+++ Frontend/ValoAPI/src/components/weapon/WeaponUI.tsx
@@ -1,6 +1,8 @@
-import React, {useState, useEffect}from 'react'
+import React, {useState, useEffect, useLayoutEffect}from 'react'
 import { WeaponType,  chromasType,  skinsType } from '../typings/weaponTypes'
 import { Chromas } from './Chromas'
+
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import { Icon } from '@iconify/react';
 import axios from 'axios';
@@ -70,7 +72,7 @@ const removeRandomSkin = (skins: skinsType[] | null) =>{
 
 export const WeaponUI : React.FC<WeaponUITypes> = ({Weapon, handleWeapon}) => {
 
-  
+  const [isHover, setIsHover] = useState(false)
 
   const WeaponData = Weapon.weaponData;
 // console.log(WeaponData, "Initial Weapon DATA")
@@ -120,12 +122,11 @@ export const WeaponUI : React.FC<WeaponUITypes> = ({Weapon, handleWeapon}) => {
   }
 
 
-  const handleDisplay = (displayname: string, image: string | null) =>{
+  const handleDisplay = (image: string | null) =>{
     SetCurrSkin(prevState => ({
       ...prevState,
-      displayName: displayname,
-      imagePath:image,
       
+      imagePath:image,
       }))
   }
 
@@ -148,8 +149,10 @@ export const WeaponUI : React.FC<WeaponUITypes> = ({Weapon, handleWeapon}) => {
   
   // console.log(Weapon, "WEAPON!");
   // console.log(WeaponData !== null && WeaponData.skins, "WEAPONS DATA!");
-  // console.log(currSkin, "Current SKIN!")
+  console.log(currSkin, "Current SKIN!")
   // console.log(currChroma, "CHROOOMAS")
+
+  
   return (
     <div className={`${!Weapon.isTrigger?"hidden":"relative"} w-full h-screen bg-ValoDarkViolet z-50 py-6`}> 
 
@@ -162,12 +165,22 @@ export const WeaponUI : React.FC<WeaponUITypes> = ({Weapon, handleWeapon}) => {
       <div className='relative  w-full h-full p-4'>
 
     
-        <label className='text-ValoGreen uppercase text-[4em] font-Tungsten tracking-widest relative z-20'>{currSkin && currSkin !== null && currSkin.displayName}</label>
+        <label className='text-ValoGreen uppercase custom-text font-Tungsten tracking-widest relative z-20'>{currSkin && currSkin !== null && currSkin.displayName}</label>
 
        
         <div className=" bg-opacity-5 relative flex justify-center h-[20em] overflow-hidden items-center">
-          <img className="h-[14em] 2xl:w-[40%] xl:w-[50%] lg:w-[80%] md:w-[90%] sm:w-[100%] object-contain relative z-10 drop-shadow-xl" src={currSkin.imagePath || undefined} alt="image description"/>
+          <img className =
+          {`h-[14em] custom-width object-contain absolute z-10 brightness-0 bottom-12 opacity-50 transition-all duration-300 ${isHover ? 'bottom-[30px]' : ''}`} 
+          src={currSkin.imagePath || undefined} alt="image description"
+          />
 
+
+          <img className="h-[14em] 2xl:w-[40%] xl:w-[50%] lg:w-[80%] md:w-[90%] sm:w-[100%] object-contain relative z-10 bottom-0 transition-all duration-300 "src={currSkin.imagePath || undefined} alt="image description"
+          
+          onMouseLeave={()=>{setIsHover(false)}}
+          onMouseEnter={()=>{setIsHover(true)}}
+          />
+          
           {currSkin && currSkin.contentTier ? <img className="h-[20em] min-w-[4em] max-w-[20em] object-contain absolute -bottom-1/4 left-[1em] opacity-50" src={currSkin.contentTier?.displayIcon || undefined} /> : ""}
             
           <Chromas chromas={currChroma} handleDisplay={handleDisplay}/>
@@ -190,15 +203,19 @@ export const WeaponUI : React.FC<WeaponUITypes> = ({Weapon, handleWeapon}) => {
           </div>
 
         
-          <div className='flex flex-row gap-2 flex-nowrap overflow-x-scroll relative p-2'>
+          <div className='flex flex-row gap-2 flex-nowrap overflow-x-scroll relative p-2 bg-black bg-opacity-25'>
 
             {WeaponData && WeaponData.skins !== null && WeaponData.skins.map((skin,id )=> (
-              <div key={id} className='relative h-fit 2xl:min-w-[24em] flex flex-col sm:min-w-[10em] md:min-w-[12em] lg:min-w-[14em] xl:min-w-[16em] hover:bg-ValoGreen' onClick={()=>{handleWeaponSkin(skin.displayName, skin.chromas[0].fullRender, skin.contentTierUuid, skin.chromas)}}>
-                <div className='flex flex-col justify-around border-2 p-1 relative'>
+              <div key={id} className='relative min-h-[1em] 2xl:min-w-[24em] flex flex-col sm:min-w-[12em] md:min-w-[13em] lg:min-w-[14em] 
+              xl:min-w-[18em] ' onClick={()=>{handleWeaponSkin(skin.displayName, skin.chromas[0].fullRender, skin.contentTierUuid, skin.chromas)}}>
+                <div className='flex flex-col justify-around border-2 p-1 relative hover:bg-ValoGreen'>
 
                   <div className=' w-full h-full relative z-20 hover:bg-gray-700 bg-gray-600 bg-opacity-5' style={{clipPath: 'polygon(15% 0, 100% 0, 100% 100%, 0 100%, 0 30%)'}}>
-                    <div className='w-full h-full py-10 hover:bg-gradient-to-t from-ValoGreen from-0% via-transparent via-90%  to-transparent to-90% transition-all duration-300'>
-                      <img  src={skin.chromas[0].fullRender} alt={skin.displayName} className='object-contain w-full h-full'/>
+                    <div className='w-full h-full py-4 hover:bg-gradient-to-t from-ValoGreen from-0% via-transparent via-90%  to-transparent to-90% transition-all duration-300'>
+
+                      <LazyLoadImage src={skin.chromas[0].fullRender} alt={skin.displayName} 
+                      className=' object-contain w-full h-[3.5em] 2xl:h-[5em] xl:h-[5em] hover:-translate-y-2 transition-all'/>
+
                     </div>
                   </div>
 
